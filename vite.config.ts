08f8969ee97dae0d1677bf76b09ee337ff8d3fa3
douglasyabuki <douglasyabuki@gmail.com>
2024-09-11 06:21:17 -0300

    Add: Config to specify used third party API (CORS)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fake-api.tractian.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
