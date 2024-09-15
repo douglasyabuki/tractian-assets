42b34a4734da3644f2a7c7c50825e3500c55260c
douglasyabuki <douglasyabuki@gmail.com>
2024-09-15 19:13:18 -0300

    Add: Hooks (useDragAndDrop, useLocalStorage)

---
+++ src/hooks/use-drag-and-drop.ts
@@ -0,0 +1,34 @@
+// hooks/useDragAndDrop.ts
+import { useState } from "react";
+
+export const useDragAndDrop = (onFileDrop: (file: File) => void) => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(true);
+  };
+
+  const onDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(false);
+  };
+
+  const onDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDragging(false);
+
+    const file = event.dataTransfer.files[0];
+    onFileDrop(file);
+  };
+
+  return {
+    isDragging,
+    onDragOver,
+    onDragLeave,
+    onDrop,
+  };
+};
---
+++ src/hooks/use-local-storage.ts
@@ -0,0 +1,71 @@
+import { safeStringify } from "@/utils/util-object";
+import { useEffect, useState } from "react";
+
+const isLocalStorageAvailable = () => {
+  try {
+    const testKey = "__storage_test__";
+    localStorage.setItem(testKey, testKey);
+    localStorage.removeItem(testKey);
+    return true;
+  } catch (err) {
+    if (err) {
+      return false;
+    }
+  }
+};
+
+export const useLocalStorage = (key: string, initialValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (!isLocalStorageAvailable) {
+      console.warn("Local storage not available");
+      return initialValue;
+    }
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (err) {
+      console.error(`Failed to read localStorage key "${key}":`, err);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    if (isLocalStorageAvailable()) {
+      try {
+        const item = localStorage.getItem(key);
+        setStoredValue(item ? JSON.parse(item) : initialValue);
+      } catch (err) {
+        console.error(`Failed to read localStorage key "${key}":`, err);
+      }
+    }
+  }, [key, initialValue]);
+
+  const setValue = (value = {}) => {
+    if (!isLocalStorageAvailable) {
+      console.warn("Local storage not available");
+      return initialValue;
+    }
+    try {
+      const valueToStore = value;
+      setStoredValue(valueToStore);
+      localStorage.setItem(key, safeStringify(valueToStore));
+    } catch (err) {
+      console.error(`Failed to set localStorage key "${key}":`, err);
+    }
+  };
+
+  const removeValue = () => {
+    if (!isLocalStorageAvailable) {
+      console.warn("Local storage not available");
+      return;
+    }
+    try {
+      localStorage.removeItem(key);
+      setStoredValue(initialValue);
+    } catch (err) {
+      console.error(`Failed to remove localStorage key "${key}":`, err);
+    }
+  };
+
+  return { storedValue, setValue, removeValue };
+};
