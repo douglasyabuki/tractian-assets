7a9592268baba6289f0967e66d890b936e643f63
douglasyabuki <douglasyabuki@gmail.com>
2024-09-15 07:30:58 -0300

    Bootstrap: Hooks (useEventListeners and useTargetRect)

---
+++ src/hooks/use-event-listeners.ts
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+
+export type EventListener<T extends keyof WindowEventMap> = {
+  eventName: T;
+  handler: (event: WindowEventMap[T]) => void;
+};
+
+type UseEventListeners<T extends keyof WindowEventMap> = {
+  eventListeners: EventListener<T>[];
+};
+
+export const useEventListeners = <T extends keyof WindowEventMap>({
+  eventListeners,
+}: UseEventListeners<T>) => {
+
+  useEffect(() => {
+    eventListeners.forEach(({ eventName, handler }) => {
+      window.addEventListener(eventName, handler);
+    });
+
+    return () => {
+      eventListeners.forEach(({ eventName, handler }) => {
+        window.removeEventListener(eventName, handler);
+      });
+    };
+  }, [eventListeners]);
+};
